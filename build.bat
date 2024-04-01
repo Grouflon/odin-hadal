ee36a7f33a2668aca3531f725ebdb6fd22a95f1a
Memuth <remi.bismuth@gmail.com>
2024-04-02 08:02:07 +0900

    [build] removing thread count limit as the compiler bug is now fixed

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ rem CREATE BIN DIR
 
 rem COMPILE
 @echo on
-odin build ./src -out:bin/hadal.exe -collection:hadal=src -warnings-as-errors -debug -thread-count:1
+odin build ./src -out:bin/hadal.exe -collection:hadal=src -warnings-as-errors -debug
 @echo off
 if %errorlevel% neq 0 goto error
 
